398685df9b04321cee532113be7da4ba42d860eb
tora <ccacrop@gmail.com>
2025-04-20 00:21:29 +0800

    add keymappings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,8 +4,12 @@
 
 -- telescope
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>1', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>2', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>3', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>4', builtin.help_tags, { desc = 'Telescope help tags' })
+local kmap = vim.keymap.set
+kmap('n', '<leader>1', builtin.find_files, { desc = 'Telescope find files' })
+kmap('n', '<leader>2', builtin.live_grep, { desc = 'Telescope live grep' })
+kmap('n', '<leader>3', builtin.buffers, { desc = 'Telescope buffers' })
+kmap('n', '<leader>4', builtin.help_tags, { desc = 'Telescope help tags' })
 
+-- exit
+kmap("n", "<leader>qq", ":q<CR>", { desc = "Exit single file" })
+kmap("n", "<leader>q<ESC>", ":qa<CR>", { desc = "Exit all file" })
